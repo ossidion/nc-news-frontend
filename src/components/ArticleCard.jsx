64bcae330e9defd58d3df6caa4a9cf2672935695
ossidion <alexinbristol@outlook.com>
2025-04-02 13:58:25 +0100

    ticket-3-view-individual-article

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,15 +1,18 @@
 import { reverseDate } from "../../utilityFunctions"
+import { Link } from "react-router-dom"
 
 function ArticleCard ({ article }) {
   return (
     <>
       <section className="article-card">
-        <p>{article.title}</p> 
-        <img className="img" src={article.article_img_url} alt={article.title}></img>
+        <Link to={`articles/${article.article_id}`}>
+          <p>{article.title}</p>
+          <img className="img" src={article.article_img_url} alt={article.title}></img>
+        </Link> 
         <p className="article-published-by">Published by {article.author}</p>
         <p className="article-created-at">{article.created_at.slice(11, -8)}     
         {" on "}{reverseDate(article.created_at)}</p>
-        <p class="article-list-votes">Votes: {article.votes}</p>
+        <p className="article-list-votes">Votes: {article.votes}</p>
       </section>
     </>
   )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,9 @@
 function Nav () {
     return (
-        <section class="nav">
-            <a class="nav-item" href="topics">topics</a>
-            <a class="nav-item" href="about">about</a>
-            <a class="nav-item" href="contact">contact</a>
+        <section className="nav">
+            <a className="nav-item" href="topics">topics</a>
+            <a className="nav-item" href="about">about</a>
+            <a className="nav-item" href="contact">contact</a>
         </section>
 
 
